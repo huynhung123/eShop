f8284448e82fab48e4f360333d4e5e38a096f5e5
trinh huy <trinhhuy23101994@gmail.com>
2021-08-31 00:21:58 +0700

    bai 24

---
+++ Tedusop.web/App/shraed/Filters/Status.filters.js
@@ -0,0 +1,17 @@
+﻿(function (app) {
+
+    app.filter('statusfilters', function () {
+
+        return function (input) {
+
+            if (input == true)
+                return  'Kich hoạt';
+            else
+                return 'Khóa';
+
+        }
+
+
+    });
+
+})(angular.module('Tedushop.common'));
