1e33c142850bdae5eae164b1b931904f8fecf4c7
trinh huy <trinhhuy23101994@gmail.com>
2021-10-06 00:14:49 +0700

    a

--- Tedusop.web/Controllers/HomeController.cs
+++ Tedusop.web/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tedusop.Model.Models;
+using Tedusop.Service;
+using Tedusop.web.Models;
 
 namespace Tedusop.web.Controllers
 {
     public class HomeController : Controller
     {
+        IProducCategoryService _producCategoryService;
+
+        public HomeController(IProducCategoryService producCategoryService)
+        {
+
+            this._producCategoryService = producCategoryService;
+        }
         // GET: Admin
         public ActionResult Index()
         {
@@ -25,7 +36,9 @@ namespace Tedusop.web.Controllers
         }
         public ActionResult category()
         {
-            return PartialView();
+            var model = _producCategoryService.GetAll();
+            var listCategory = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+            return PartialView(listCategory);
         }
     }
 }
